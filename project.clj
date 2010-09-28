53e7cdae378c2c236b55bb39d179c83bd9f240b0
Alan Dipert <alan@dipert.org>
2010-09-27 23:54:24 -0400

    bump to 1.0.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject reconfig "1.0.1"
+(defproject reconfig "1.0.2"
   :description "Catch signals and reload a config file"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
